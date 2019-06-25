545a269aa19772200c4dc560e6f94efcb3ea0222
Gnom-New <multibrend01@gmail.com>
2019-06-25 12:47:10 +0200

    New prodzect chess

---
+++ chess/Figure.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace chess
+{
+    enum Figure
+    {
+        none,
+        whiteKing = 'K',
+        whiteQeen = 'Q',
+        whiteRook = 'R',
+        whiteBishop = 'B',
+        whiteKnight = 'N',
+        whitePaw = 'P',
+
+        blackKing = 'k',
+        blackQeen = 'q',
+        blackRook = 'r',
+        blackBishop = 'b',
+        blackKnight = 'n',
+        blackPaw = 'p',
+    }
+
+    static class FigureMethods
+    {
+        public static Color GetColor(this Figure figure)
+        {
+            if (figure == Figure.none)
+                return Color.none;
+            return (figure == Figure.whiteKing ||
+                    figure == Figure.whiteQeen ||
+                    figure == Figure.whiteRook ||
+                    figure == Figure.whiteBishop ||
+                    figure == Figure.whiteKnight ||
+                    figure == Figure.whitePaw)
+                    ? Color.white
+                    : Color.black;
+        }
+    }
+}
